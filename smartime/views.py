c54078bedac8108c98778db5da882ed6d0731d5a
Lourdes Montes <vanuuchan16@gmail.com>
2016-11-25 00:41:05 -0500

    SOlo faltan vistas completas y ya

--- smartime/views.py
+++ smartime/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from smartime.forms import AlarmForm, SettingForm, EventForm, HomeworkForm
 from .models import User, Alarm, Setting, Event, Homework
 
